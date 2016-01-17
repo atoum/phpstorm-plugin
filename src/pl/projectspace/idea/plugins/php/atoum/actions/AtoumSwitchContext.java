27d0c4f8be2199b529f6a261a0862df0efd66e78
Adrien Gallou <adriengallou@gmail.com>
2016-01-17 18:37:26 +0100

    add actions to run the current test

--- src/pl/projectspace/idea/plugins/php/atoum/actions/AtoumSwitchContext.java
+++ src/pl/projectspace/idea/plugins/php/atoum/actions/AtoumSwitchContext.java
@@ -2,13 +2,10 @@ package pl.projectspace.idea.plugins.php.atoum.actions;
 
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.jetbrains.php.PhpIndex;
 import com.jetbrains.php.lang.psi.PhpFile;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
 import org.jetbrains.annotations.Nullable;
 import com.intellij.ide.actions.OpenFileAction;
-import java.util.Collection;
 
 public class AtoumSwitchContext extends AnAction {
 
@@ -32,6 +29,7 @@ public class AtoumSwitchContext extends AnAction {
         if (null == testedClass) {
             return;
         }
+
         OpenFileAction.openFile(testedClass.getContainingFile().getVirtualFile().getPath(), e.getProject());
     }
 
@@ -58,57 +56,15 @@ public class AtoumSwitchContext extends AnAction {
 
     @Nullable
     protected PhpClass getSwitchClass(Project project, PhpFile phpFile) {
-        PhpClass currentClass = getFirstClassFromFile(phpFile);
+        PhpClass currentClass = Utils.getFirstClassFromFile(phpFile);
         if (null == currentClass) {
             return null;
         }
 
-        if (isClassAtoumTest(currentClass)) {
-            return locateTestedClass(project, currentClass);
+        if (Utils.isClassAtoumTest(currentClass)) {
+            return Utils.locateTestedClass(project, currentClass);
         }
 
-        return locateTestClass(project, currentClass);
+        return Utils.locateTestClass(project, currentClass);
     }
-
-    protected Boolean isClassAtoumTest(PhpClass checkedClass)
-    {
-        return checkedClass.getNamespaceName().endsWith(getTestsNamespaceSuffix());
-    }
-
-    @Nullable
-    protected PhpClass locateTestClass(Project project, PhpClass testedClass) {
-        String testClassname = testedClass.getNamespaceName() + getTestsNamespaceSuffix() + testedClass.getName();
-        return locatePhpClass(project, testClassname);
-    }
-
-    @Nullable
-    protected PhpClass locateTestedClass(Project project, PhpClass testClass) {
-        String testClassNamespaceName = testClass.getNamespaceName();
-        String testedClassname = testClassNamespaceName.substring(0, testClassNamespaceName.length() - getTestsNamespaceSuffix().length()) + testClass.getName();
-        return locatePhpClass(project, testedClassname);
-
-    }
-
-    @Nullable
-    protected PhpClass locatePhpClass(Project project, String name) {
-        Collection<PhpClass> phpClasses = PhpIndex.getInstance(project).getAnyByFQN(name);
-        if (phpClasses.size() != 1) {
-            return null;
-        }
-
-        return (PhpClass)phpClasses.toArray()[0];
-    }
-
-    private String getTestsNamespaceSuffix()
-    {
-        return "tests\\units\\";
-    }
-
-    //https://github.com/Haehnchen/idea-php-symfony2-plugin/blob/cb422db9779025d65fdf0ba5d26a38d401eca939/src/fr/adrienbrault/idea/symfony2plugin/util/PhpElementsUtil.java#L784
-    @Nullable
-    private PhpClass getFirstClassFromFile(PhpFile phpFile) {
-        Collection<PhpClass> phpClasses = PsiTreeUtil.collectElementsOfType(phpFile, PhpClass.class);
-        return phpClasses.size() == 0 ? null : phpClasses.iterator().next();
-    }
-
 }
